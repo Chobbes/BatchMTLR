4c62b1fa312ba6e26ed10cf4b73d6c0360571da9
Calvin Beck <hobbes@ualberta.ca>
2014-11-24 21:50:13 -0700

    Initial commit.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc783
+}:
+
+haskellPackages.cabal.mkDerivation (self: {
+  pname = "BashMTLR";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = with haskellPackages; [  ];
+  buildTools = with haskellPackages; [ cabalInstall ];
+  meta = {
+    homepage = "https://github.com/Chobbes/BatchMTLR";
+    description = "Batch running of MTLR training for patient specific survival prediction.";
+    license = self.stdenv.lib.licenses.mit;
+    platforms = self.ghc.meta.platforms;
+  };
+})
