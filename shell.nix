af02c169923ec08276002212da81277c726f88f4
Calvin Beck <hobbes@ualberta.ca>
2014-11-25 21:18:28 -0700

    Updated dependencies...

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ haskellPackages.cabal.mkDerivation (self: {
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = with haskellPackages; [ directoryTree ];
+  buildDepends = with haskellPackages; [ directoryTree cmdargs ];
   buildTools = with haskellPackages; [ cabalInstall ];
   meta = {
     homepage = "https://github.com/Chobbes/BatchMTLR";
